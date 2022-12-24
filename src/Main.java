78aa7239bee082d476b5e840715567a8a6e2625e
Sanchitha Udana Wijesundara <udanasanchitha4@gmail.com>
2022-12-24 14:53:55 +0530

    First Commit

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,13 @@ public class Main {
     public static void main(String[] args) {
 
         // --> Stack Data Structure
-        // this is the "last in first out"
+            // this is the "last in first out"
 
             // create a stack
         Stack<String> stackOfBooks = new Stack<>();
             // check stack is empty or not
         System.out.println("before pushing "+stackOfBooks.empty());
-
+        System.out.println("is Stack empty ? "+stackOfBooks.isEmpty());
             // pushing data to the Stack
         stackOfBooks.push("python notes");
         stackOfBooks.push("java notes");
@@ -35,10 +35,13 @@ public class Main {
             // view the value in top of the stack
         String topOfItem = stackOfBooks.peek();
         System.out.println("the top of value : " + topOfItem);
+        // or
+        System.out.println(stackOfBooks.peek());
 
+//----------------------------------------------------------------------------------------------------------------------//
 
         // --> Queue Data Structure
-        // this is the "first in first out"
+            // this is the "first in first out"
 
         Queue<String> newQueue = new LinkedList<>();
 
@@ -47,6 +50,7 @@ public class Main {
         newQueue.offer("wijesundara");
         newQueue.offer("vilayaya");
 
+            // we can check queue is empty or not
         System.out.println(newQueue.isEmpty());
             // we can check size of the queue
         System.out.println(newQueue.size());
@@ -58,8 +62,12 @@ public class Main {
         System.out.println(newQueue);
         newQueue.poll();
         System.out.println(newQueue);
+            // clear alla data included in Queue
+        newQueue.clear();
+        System.out.println(newQueue);
+        
 
-
+//----------------------------------------------------------------------------------------------------------------------//
 
 
 
